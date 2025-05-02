70ce49018197af04d0a643a0911e4f489f6c21d7
Hrvoje Pavlinovic <hpavlino@fesb.hr>
2025-05-02 22:19:01 +0200

    feat: update homepage with social links and profile picture

--- routes/index.tsx
+++ routes/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       </p>
       <div class="flex gap-6">
         <a 
-          href="https://x.com/0xhp10"
+          href="https://twitter.com/hrvoje_eth" 
           target="_blank" 
           rel="noopener noreferrer"
           class="dark:text-white/60 dark:hover:text-[#F7931A] text-black/60 hover:text-[#F7931A]"
@@ -21,13 +21,22 @@ export default function Home() {
           </svg>
         </a>
         <a 
-          href="https://www.linkedin.com/in/hpavlino" 
+          href="https://www.linkedin.com/in/hrvojepavlinovic" 
           target="_blank" 
           rel="noopener noreferrer"
           class="dark:text-white/60 dark:hover:text-[#F7931A] text-black/60 hover:text-[#F7931A]"
         >
           <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
             <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"/>
+          </svg>
+        </a>
+        <a 
+          href="mailto:hrvoje@pavlinovic.com"
+          class="dark:text-white/60 dark:hover:text-[#F7931A] text-black/60 hover:text-[#F7931A]"
+        >
+          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z"/>
+            <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z"/>
           </svg>
         </a>
       </div>
