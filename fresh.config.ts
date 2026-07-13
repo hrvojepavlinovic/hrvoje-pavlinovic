fde9e1163354a234a946e91a977f49908e32419f
Hrvoje Pavlinovic <hpavlino@fesb.hr>
2026-07-13 16:41:37 +0200

    Bind Fresh server to localhost

--- fresh.config.ts
+++ fresh.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 
 export default defineConfig({
+  server: { hostname: "127.0.0.1" },
   plugins: [tailwind()],
 });
